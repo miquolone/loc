8d145211aeb45cb3154c3f129acca90ad1a11042
takagi <bomibako@gmail.com>
2018-10-27 03:11:10 +0900

    a

--- src/index.js
+++ src/index.js
@@ -11,18 +11,21 @@ function component() {
 
 document.body.appendChild(component());
 
-/* トップ画面*/
-if (location.pathname == "/") {
-  var itemList = document.querySelector('#itemList');
-  var template = document.getElementById('ItemTemplate');
+(function(){
+  /* トップ画面*/
+  if (location.pathname == "/") {
+  console.log('koko');
+    var itemList = document.querySelector('#itemList');
+    var template = document.getElementById('ItemTemplate');
 
-  for (i=0; i < 8; i++ ) {
-    var clone = document.importNode(template.content, true);
-    clone.querySelector('img').src = `/images/shopItem0${i}.png`;
-    itemList.appendChild(clone);
+    for (i=0; i < 8; i++ ) {
+      var clone = document.importNode(template.content, true);
+      clone.querySelector('img').src = `/images/shopItem0${i}.png`;
+      itemList.appendChild(clone);
+    }
   }
-}
 
-/* 決済選択画面 */
-if (location.pathname == "selectpayment.html") {
-}
+  /* 決済選択画面 */
+  if (location.pathname == "selectpayment.html") {
+  }
+})
