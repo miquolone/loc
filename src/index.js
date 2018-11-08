269c18b2bb76316d8b3f773e9b42309181e1da34
takagi <bomibako@gmail.com>
2018-11-08 23:43:38 +0900

    a

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 
 const bootstrap = require('bootstrap');
-//const THREE = require('three');
-import THREE from 'three';
+const THREE = require('three');
+//import THREE from 'three';
 const PhotoSphereViewer = require('photo-sphere-viewer');
 const jquery = require('jquery');
 const request = require('superagent');
